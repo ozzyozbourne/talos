009b98dced60fa897727ef734dfe705a0b2437be
ozzy ozbourne <khanosaid726@gmail.com>
2023-01-03 23:13:45 +0530

    added more features

--- src/test/java/sample/SampleTest.java
+++ src/test/java/sample/SampleTest.java
@@ -7,5 +7,9 @@ public class SampleTest {
 
     public void testA(){
         System.out.println("Hello Gradle Testng");
+        System.out.println(System.getProperty("tier"));
+        System.out.println(System.getProperty("browser"));
+        System.out.println(System.getProperty("product"));
+
     }
 }
