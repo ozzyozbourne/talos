eada78bd95a0ded30bbd952f3170697abc16938f
ozzy ozbourne <khanosaid726@gmail.com>
2023-01-03 15:36:18 +0530

    talos automaton

---
+++ src/test/java/sample/SampleTest.java
@@ -0,0 +1,11 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+@Test
+public class SampleTest {
+
+    public void testA(){
+        System.out.println("Hello Gradle Testng");
+    }
+}
---
+++ src/test/java/sample/SampleTest2.java
@@ -0,0 +1,14 @@
+package sample;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+@Test
+public class SampleTest2 {
+
+    public void testA(){
+
+        Reporter.log("sdfksdhfwoieuf");
+        System.out.println("Hello Gradle Testng threaded");
+    }
+}
