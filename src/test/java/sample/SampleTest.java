134248526f6269266c96bfabc9fa0054266a7034
ozzy ozbourne <khanosaid726@gmail.com>
2023-01-04 02:12:31 +0530

    playwright added

--- src/test/java/sample/SampleTest.java
+++ src/test/java/sample/SampleTest.java
@@ -1,5 +1,8 @@
 package sample;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
 import org.testng.annotations.Test;
 
 @Test
@@ -11,5 +14,14 @@ public class SampleTest {
         System.out.println(System.getProperty("browser"));
         System.out.println(System.getProperty("product"));
 
+    }
+
+    public void playwright(){
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch();
+            Page page = browser.newPage();
+            page.navigate("http://playwright.dev");
+            System.out.println(page.title());
+        }
     }
 }
