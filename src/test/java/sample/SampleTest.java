a05eccb0fffda183038aa84e78c82ff0aecc2e02
ozzy ozbourne <khanosaid726@gmail.com>
2023-01-04 07:45:53 +0530

    more config added

--- src/test/java/sample/SampleTest.java
+++ src/test/java/sample/SampleTest.java
@@ -13,15 +13,18 @@ public class SampleTest {
         System.out.println(System.getProperty("tier"));
         System.out.println(System.getProperty("browser"));
         System.out.println(System.getProperty("product"));
+        System.out.println(System.getProperty("server"));
+        System.out.println(System.getenv("SELENIUM_REMOTE_URL"));
+        System.out.println(System.getenv("PLAYWRIGHT_BROWSERS_PATH"));
 
     }
 
-    public void playwright(){
-        try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch();
-            Page page = browser.newPage();
-            page.navigate("http://playwright.dev");
-            System.out.println(page.title());
-        }
-    }
+//    public void playwright(){
+//        try (Playwright playwright = Playwright.create()) {
+//            Browser browser = playwright.chromium().launch();
+//            Page page = browser.newPage();
+//            page.navigate("http://playwright.dev");
+//            System.out.println(page.title());
+//        }
+//    }
 }
