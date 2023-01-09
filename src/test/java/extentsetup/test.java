61a358b6d4dffbc16ac5e4b33af9602ea14059e5
ozzy ozbourne <khanosaid726@gmail.com>
2023-01-09 20:39:56 +0530

    adding db support

--- src/test/java/extentsetup/test.java
+++ src/test/java/extentsetup/test.java
@@ -1,21 +1,59 @@
 package extentsetup;
 
 import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.markuputils.CodeLanguage;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-import com.aventstack.extentreports.reporter.configuration.Theme;
+import com.aventstack.extentreports.reporter.configuration.ViewName;
+import com.google.common.collect.ImmutableMap;
 import lombok.val;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class test {
 
+    private static final String reportPath = "extentreports/index.html";
+    private static final String configPath = "src/main/resources/extentconfig/extentconfig.json";
     @Test
-    public void test(){
-        // directory where output is to be printed
-        val spark = new ExtentSparkReporter("extentsetup/index.html");
-        val extent = new ExtentReports();
-        extent.attachReporter(spark);
-        spark.config().setTheme(Theme.DARK);
-        spark.config().setDocumentTitle("My Report");
+    public void test() throws IOException, InterruptedException {
 
+        val spark = new ExtentSparkReporter(reportPath)
+                .viewConfigurer().viewOrder().as(new ViewName[]{ViewName.DASHBOARD, ViewName.TEST}).apply();
+        val extent = new ExtentReports();
+        val CONF = new File(configPath);
+        spark.loadJSONConfig(CONF);
+
+        extent.attachReporter(spark);
+
+        val extentTest = extent.createTest("lofing test")
+                .assignAuthor("sdkf").assignCategory("smolke").assignDevice("desktop");
+        extentTest.pass("SUcessfult");
+        extentTest.pass("Url loaded successfullt");
+        extentTest.pass(MarkupHelper.createCodeBlock("", CodeLanguage.XML));
+        val extentTest1 = extent.createTest("lofing test");
+        extentTest1.fail("sdfklskd");
+        extentTest1.info("ldkfhlskdv");
+        Thread.sleep(10000L);
+        val extentTest2 = extent.createTest("lofing test");
+        extentTest2.fail(MarkupHelper.createTable(new String[][]{{"afd"}, {"sdvkn"}}));
+        extentTest2.fail(MarkupHelper.createLabel("WOho ", ExtentColor.RED));
+        extentTest2.fail(MarkupHelper.createOrderedList(List.of("sdvj", "sdfh")));
+        extentTest2.fail(MarkupHelper.createUnorderedList(ImmutableMap.of("sdvj", "sdfh", "rfef", "wewf")));
+        val re = extentTest2.createNode("new node");
+        re.info("sldkfj");
+        extent.flush();
+
+    }
+
+    @Test
+    public void testNgReprtedtest(){
+       val s =  MarkupHelper.createOrderedList(List.of("sdvj", "sdfh")).getMarkup();
+        System.out.println(s);
+        Reporter.log(s);
     }
 }
