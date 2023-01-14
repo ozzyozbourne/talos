896c2a8b0542bc19b6e7719b7f8bc98bca071dd5
ozzy ozbourne <khanosaid726@gmail.com>
2023-01-14 18:13:24 +0530

    poiji reader added

--- src/main/java/org/example/readers/PoijiXl.java
+++ src/main/java/org/example/readers/PoijiXl.java
@@ -1,7 +1,23 @@
 package org.example.readers;
 
+import com.poiji.bind.Poiji;
+import com.poiji.option.PoijiOptions;
+import org.example.framework.Constants;
+
+import java.io.File;
+import java.util.List;
+
 public final class PoijiXl {
+
+    private final static String PATH_TO_DIR = Constants.PATH_TEST_RC + "XLSX" + File.separator;
 
     private PoijiXl() {
     }
+
+    public static <T> List<T> getPojoFromXlsx(String fileName, String sheetName, Class<T> t) {
+        String path = PATH_TO_DIR + fileName + ".xlsx";
+        final PoijiOptions options = PoijiOptions.PoijiOptionsBuilder.settings().sheetName(sheetName).build();
+        return Poiji.fromExcel(new File(path), t, options);
+    }
+
 }
