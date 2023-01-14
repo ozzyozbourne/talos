829af7ecb6640747b103ba914b35e2e47f1dbdd3
ozzy ozbourne <khanosaid726@gmail.com>
2023-01-14 22:30:54 +0530

    csv reader fixed

--- src/main/java/org/example/readers/CSV.java
+++ src/main/java/org/example/readers/CSV.java
@@ -1,33 +1,35 @@
 package org.example.readers;
 
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.csv.CsvGenerator;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
-import org.example.framework.Constants;
+import lombok.val;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Supplier;
 
 public class CSV {
-
-    private final static String PATH_TO_DIR = Constants.PATH_TEST_RC + "CSV" + File.separator;
 
     private CSV() {
     }
 
-    public static <T> Optional<List<T>> getPojoCsvList(final String fileName, final Class<T> t) throws IOException {
-
-        String path = PATH_TO_DIR + fileName + ".csv";
-        final CsvMapper csvMapper = new CsvMapper();
-        final CsvSchema schema = csvMapper.schemaFor(t).withSkipFirstDataRow(true).withColumnSeparator('|');
+    public static <T> Optional<List<T>> getPojoCsvList(final String filePath, final Class<T> t, final CsvSchema schema) throws IOException {
+        final CsvMapper csvMapper = CsvMapper.csvBuilder().build();
         Optional<List<T>> optionalTList;
 
-        try(MappingIterator<T> iterator = csvMapper.readerFor(t).with(schema).readValues(new File(path))){
+        try(MappingIterator<T> iterator = csvMapper.readerFor(t).with(schema).readValues(new File(filePath))){
             optionalTList = Optional.of(iterator.readAll());
         }
         return optionalTList;
     }
+
+    public static final Supplier<CsvSchema> getPgObjCSVSchema = () -> CsvSchema.builder().addColumn("ELEMENT_NAME").addColumn("TYPE")
+            .addColumn("LOCATOR_VALUE").setSkipFirstDataRow(true).setColumnSeparator('|').build();
 
 }
--- src/test/java/frameworktests/CsvReaderTests.java
+++ src/test/java/frameworktests/CsvReaderTests.java
@@ -1,8 +1,43 @@
 package frameworktests;
 
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import lombok.val;
+import org.assertj.core.api.Assertions;
+import org.example.framework.Constants;
+import org.example.pojos.PgObjCSV;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.example.readers.CSV.getPgObjCSVSchema;
+import static org.example.readers.CSV.getPojoCsvList;
 
 @Test
 public final class CsvReaderTests {
+
+    final static String PATH_TO_DIR = Constants.PATH_TEST_RC + "CSVFiles" + File.separator;
+
+    public void CSVTestOne() throws IOException {
+
+        val loc = PATH_TO_DIR + "test.csv";
+        val list = getPojoCsvList(loc, PgObjCSV.class, getPgObjCSVSchema.get());
+        Assertions.assertThat(list).isPresent();
+        val act = list.get();
+        act.forEach(s -> System.out.println(s.getElementName().trim() + "\t" + s.getType().trim() + "\t" +
+                s.getLocatorValue().trim()));
+
+        val lOne = act.get(0);
+        val lTwo = act.get(1);
+
+        Assert.assertEquals(lOne.getElementName().trim(), "UserName");
+        Assert.assertEquals(lOne.getType().trim(), "xpath");
+        Assert.assertEquals(lOne.getLocatorValue().trim(), "//h1[@class = 'heello']");
+
+        Assert.assertEquals(lTwo.getElementName().trim(), "Password");
+        Assert.assertEquals(lTwo.getType().trim(), "xpath");
+        Assert.assertEquals(lTwo.getLocatorValue().trim(), "//a[contains(text(), 'yello')]");
+    }
 
 }
