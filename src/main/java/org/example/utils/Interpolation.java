95dff5386a27d698f4d9de25ddb48244380e35b7
ozzy ozbourne <khanosaid726@gmail.com>
2023-01-14 19:11:58 +0530

    interpolation

--- src/main/java/org/example/utils/Interpolation.java
+++ src/main/java/org/example/utils/Interpolation.java
@@ -1,4 +1,21 @@
 package org.example.utils;
 
-public class Interpolation {
+import com.google.common.collect.ImmutableMap;
+import org.apache.commons.text.StringSubstitutor;
+
+public final class Interpolation {
+
+    public static String replaceOne(final String input, final String replace){
+        return new StringSubstitutor(ImmutableMap.of("0", replace)).replace(input);
+    }
+
+    public static String replaceTwo(final String input, final String replaceOne, final String replaceTwo){
+        return new StringSubstitutor(ImmutableMap.of("0", replaceOne, "1", replaceTwo)).replace(input);
+    }
+
+    public static String replaceThree(final String input, final String replaceOne, final String replaceTwo, final String replaceThree){
+        return new StringSubstitutor(ImmutableMap.of("0", replaceOne, "1", replaceTwo, "2", replaceThree))
+                .replace(input);
+    }
+
 }
