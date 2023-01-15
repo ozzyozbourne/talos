3dd472c49a5ff42fe8a16b990e23234e12c5ef59
ozzy ozbourne <khanosaid726@gmail.com>
2023-01-15 20:21:45 +0530

    completed xlsx page obj repo setup

--- src/test/java/frameworktests/PoijiXlsxReaderTests.java
+++ src/test/java/frameworktests/PoijiXlsxReaderTests.java
@@ -1,21 +1,26 @@
 package frameworktests;
 
 import lombok.val;
+import org.apache.poi.openxml4j.exceptions.InvalidOperationException;
 import org.example.framework.Constants;
 import org.example.pojos.PgOjbXLSX;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Collections;
 import java.util.List;
 
+import static org.example.framework.Constants.TIER;
 import static org.example.readers.PoijiXl.getPojoFromXlsx;
+import static org.example.utils.Interpolation.getLocatorXLSX;
 
 @Test
 public final class PoijiXlsxReaderTests {
 
     final static String PATH_TO_DIR = Constants.PATH_TEST_RC + "XLSXFiles" + File.separator;
+
 
     public void PoiJiXLSXReaderTestOne(){
         val loc = PATH_TO_DIR + "testpage.xlsx";
@@ -54,10 +59,47 @@ public final class PoijiXlsxReaderTests {
         pojolsit.forEach(one -> Assert.assertTrue(expList.contains(one.getLocatorValue())));
     }
 
+
     public void PoiJiXLSXReaderTestFive(){
         val loc = PATH_TO_DIR + "testpage.xlsx";
         val pojolsit = getPojoFromXlsx(loc, "two", PgOjbXLSX.class);
         val expList = Collections.singletonList("two");
         pojolsit.forEach(one -> Assert.assertTrue(expList.contains(one.getType())));
     }
+
+    @Test(expectedExceptions = InvalidOperationException.class)
+    public void PoiJiXLSXReaderTestSix(){
+        val loc = PATH_TO_DIR + "wrongpah.xlsx";
+        val pojolsit = getPojoFromXlsx(loc, "two", PgOjbXLSX.class);
+    }
+
+    public void xlsxPageObjRepoTestOne(){
+        val res = getLocatorXLSX( "testpage", "two", "OnlyCommon", "ONE", "TWO");
+        System.out.println(res);
+        Assert.assertEquals(res.left, "two");
+        Assert.assertEquals(res.right, "TWO commons only ONE ONEdev");
+
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void xlsxPageObjRepoTestTwo(){
+        val res = getLocatorXLSX( "testpage", "two", "OnlyCommons", "ONE", "TWO");
+    }
+
+    public void xlsxPageObjRepoTestThree(){
+        val res = getLocatorXLSX( "testpage", "two", "Sign-in");
+        System.out.println(res);
+        Assert.assertEquals(res.left, "two");
+        Assert.assertEquals(res.right, "#singinbutton common");
+
+    }
+
+    public void xlsxPageObjRepoTestFour(){
+        val res = getLocatorXLSX( "testpage", "two", "Four", "O", "!", "@");
+        System.out.println(res);
+        Assert.assertEquals(res.left, "two");
+        Assert.assertEquals(res.right, "@@O@!!Odev");
+
+    }
+
 }
