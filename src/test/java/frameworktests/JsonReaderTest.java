4a206734299d81815ba8c4a92f913506d0fb5998
ozzy ozbourne <khanosaid726@gmail.com>
2023-01-15 18:41:37 +0530

    added more tests

---
+++ src/test/java/frameworktests/JsonReaderTest.java
@@ -0,0 +1,12 @@
+package frameworktests;
+
+import org.example.framework.Constants;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+@Test
+public final class JsonReaderTest {
+
+    final static String PATH_TO_DIR = Constants.PATH_TEST_RC + "JSONFiles" + File.separator;
+}
--- src/test/java/frameworktests/PoijiXlsxReaderTests.java
+++ src/test/java/frameworktests/PoijiXlsxReaderTests.java
@@ -7,6 +7,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.List;
 
 import static org.example.readers.PoijiXl.getPojoFromXlsx;
 
@@ -35,5 +37,27 @@ public final class PoijiXlsxReaderTests {
         Assert.assertTrue(pojolsit.size() != 0);
         pojolsit.forEach(one -> System.out.println(one.getElementName() + "\t" + one.getType() + "\t" + one.getLocatorValue()));
         Assert.assertEquals(pojolsit.get(0).getType(), "two");
+    }
+
+    public void PoiJiXLSXReaderTestThree(){
+        val loc = PATH_TO_DIR + "testpage.xlsx";
+        val pojolsit = getPojoFromXlsx(loc, "two", PgOjbXLSX.class);
+        val expList = List.of("UserName", "Password", "Sign-in", "OnlyCommon", "Four");
+        pojolsit.forEach(one -> Assert.assertTrue(expList.contains(one.getElementName())));
+    }
+
+    public void PoiJiXLSXReaderTestFour(){
+        val loc = PATH_TO_DIR + "testpage.xlsx";
+        val pojolsit = getPojoFromXlsx(loc, "two", PgOjbXLSX.class);
+        val expList = List.of("//h1[@class = 'heello']", "//a[contains(text(), 'yello')]common", "#singinbutton common",
+                "${1} commons only ${0} ${0}", "${2}${2}${0}${2}${1}${1}${0}");
+        pojolsit.forEach(one -> Assert.assertTrue(expList.contains(one.getLocatorValue())));
+    }
+
+    public void PoiJiXLSXReaderTestFive(){
+        val loc = PATH_TO_DIR + "testpage.xlsx";
+        val pojolsit = getPojoFromXlsx(loc, "two", PgOjbXLSX.class);
+        val expList = Collections.singletonList("two");
+        pojolsit.forEach(one -> Assert.assertTrue(expList.contains(one.getType())));
     }
 }
