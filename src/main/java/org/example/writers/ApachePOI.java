40365fbec9384a090f3c700d4c53b326ddeb66a3
ozzy ozbourne <khanosaid726@gmail.com>
2023-01-23 14:36:30 +0530

    more writer utils added

--- src/main/java/org/example/writers/ApachePOI.java
+++ src/main/java/org/example/writers/ApachePOI.java
@@ -1,25 +1,41 @@
 package org.example.writers;
 
+import com.google.common.collect.Multimap;
 import lombok.val;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public final class ApachePOI {
 
     private ApachePOI() {
     }
 
-    public static synchronized void writeToXlsx(final String fileName, final String sheetName, int row, int col, final String value) throws IOException, InvalidFormatException {
+    public static void writeToNewXlsx(final String fileName, final String sheetName, final Multimap<Integer, String> valueMap) throws IOException {
        try(val fos = new FileOutputStream(fileName);val workbook = new XSSFWorkbook()){
             val sheet = workbook.createSheet(sheetName);
-            val poiRow = sheet.createRow(row);
-            val poiCol = poiRow.createCell(col, CellType.STRING);
-            poiCol.setCellValue(value);
+            val map  = valueMap.asMap();
+            map.forEach((row, col) ->{val poiRow = sheet.createRow(row); val atomicInt = new AtomicInteger(0);
+                col.forEach(c ->poiRow.createCell(atomicInt.getAndIncrement(), CellType.STRING).setCellValue(c));});
             workbook.write(fos);
        }
+    }
+
+    public static synchronized void updateCellXlsx(final String fileName, final String sheetName, int row, int col, final String value) throws IOException {
+        val fis = new FileInputStream(fileName);
+        val workbook = WorkbookFactory.create(fis);
+        workbook.getSheet(sheetName).getRow(row)
+                .createCell(col, CellType.STRING).setCellValue(value);
+        fis.close();
+        val fos = new FileOutputStream(fileName);
+        workbook.write(fos);
+        workbook.close();
+        fos.close();
     }
 }
--- src/test/java/frameworktests/ApachePOITests.java
+++ src/test/java/frameworktests/ApachePOITests.java
@@ -1,21 +1,21 @@
 package frameworktests;
 
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.example.framework.Constants;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
 
-import static org.example.writers.ApachePOI.writeToXlsx;
+import static org.example.writers.ApachePOI.updateCellXlsx;
+
 
 @Test
 public class ApachePOITests {
 
     final static String FILE_PATH = Constants.PATH_TEST_RC + "XLSXFiles" + File.separator + "poiwriter.xlsx";
 
-    public void writeToXlsxOne() throws IOException, InvalidFormatException {
-        writeToXlsx(FILE_PATH, "three", 0, 0, "EA ONE");
+    public void writeToXlsxOne() throws IOException {
+        updateCellXlsx(FILE_PATH, "three", 0, 1, "EA TWO");
     }
 
 
