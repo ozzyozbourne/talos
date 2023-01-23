efcb36533f405c4df17e36b43ef379d591ee9100
ozzy ozbourne <khanosaid726@gmail.com>
2023-01-23 17:40:53 +0530

    updated the writers

--- src/test/java/frameworktests/ApachePOITests.java
+++ src/test/java/frameworktests/ApachePOITests.java
@@ -1,5 +1,6 @@
 package frameworktests;
 
+import lombok.val;
 import org.example.framework.Constants;
 import org.testng.annotations.Test;
 
@@ -7,6 +8,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import static org.example.readers.ApachePOI.readAllFromXlsx;
 import static org.example.writers.ApachePOI.updateCellXlsx;
 import static org.example.writers.ApachePOI.updateRowXlsx;
 
@@ -16,6 +18,12 @@ public class ApachePOITests {
 
     final static String FILE_PATH = Constants.PATH_TEST_RC + "XLSXFiles" + File.separator + "poiwriter.xlsx";
     final static String FILE_PATH_TWO = Constants.PATH_TEST_RC + "XLSXFiles" + File.separator + "updaterow.xlsx";
+    final static String FILE_PATH_THREE = Constants.PATH_TEST_RC + "XLSXFiles" + File.separator + "testpage.xlsx";
+
+    @Test(expectedExceptions = NullPointerException.class)
+    public void writeToXlsxZero() throws IOException {
+        updateCellXlsx(FILE_PATH, "three", 1, 0, "EA ZERO");
+    }
 
     public void writeToXlsxOne() throws IOException {
         updateCellXlsx(FILE_PATH, "three", 0, 1, "EA TWO");
@@ -37,6 +45,11 @@ public class ApachePOITests {
 
     public void writeToXlsxSix() throws IOException {
         updateRowXlsx(FILE_PATH_TWO, "one", 0, List.of("A", "B"));
+    }
+
+    public void readAllFromXlsxTestOne() throws IOException {
+        val map  = readAllFromXlsx(FILE_PATH_THREE, "two");
+        map.asMap().forEach((k, v) -> System.out.println(k + "\t" + v));
     }
 
 }
